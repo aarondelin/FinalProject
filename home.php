635891d76b2dc2a345764d5eca77deebd50641ef
aarondelin <44951801+aarondelin@users.noreply.github.com>
2018-11-11 16:11:44 -0500

    Create home.php

---
+++ home.php
@@ -0,0 +1,24 @@
+<?php
+include ('header.php');
+?>
+<html lang="en">
+    
+    <head>
+        <title>Wavelength</title>
+        <meta charset="utf-8">
+        <meta name="author" content="Aaron Delin/Bridget Lavelle">
+        <meta name="description" content="Pop Culture">
+    </head>
+        
+    <h1></h1>
+    <p>Navigate:</p>
+    <ul>
+        <li><a href="FinalProject/Videos">Music Videos</a></li>
+        <li><a href="FinalProject/Topcharts">Top Charts</a></li>
+        <li><a href="FinalProject/Upcomingreleases">Upcoming Releases</a></li>
+        <li><a href="FinalProject/Reviews">Reviews</a></li>
+        <li><a href="FinalProject/News">News</a></li>
+        <li><a href="FinalProject/Subscribe">Sign Up</a></li>
+    </ul>
+    
+    
